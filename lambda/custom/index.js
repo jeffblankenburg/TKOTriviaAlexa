73bc310a56ac531b2a34bccd14b2ece393953c3d
Jeff Blankenburg <github@jeffblankenburg.com>
2019-07-31 16:57:32 -0400

    Handling all upsell conditions now.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,26 +8,28 @@ const dashbot = require("dashbot")(process.env.dashbot_key).alexa;
 
 var IsFirstVisit = true;
 
-const categories = [{"name": "Art & Stage", "id": "recl0vhIOs1iSyXAu", "speechName": "art and stage", "referenceName": "art_and_stage"},
-                    {"name": "Business World", "id": "recAJm9yEqDRGmjRr", "speechName": "business world", "referenceName": "business_world"},
-                    {"name": "Design", "id": "recYsOA65RT5dQPlb", "speechName": "design", "referenceName": "design"},
-                    {"name": "Film", "id": "recdMJliTRpWYcj41", "speechName": "film", "referenceName": "film"},
-                    {"name": "Food & Drink", "id": "reclySZR1cRRl08jz", "speechName": "food and drink", "referenceName": "food_and_drink"},
-                    {"name": "Geography", "id": "recXXedb9nOqy1Fkk", "speechName": "geography", "referenceName": "geography"},
-                    {"name": "History", "id": "rec6fDO7E7lXC2xe3", "speechName": "history", "referenceName": "history"},
-                    {"name": "Language", "id": "recnhcldrp4o6QwFr", "speechName": "language", "referenceName": "language"},
-                    {"name": "Literature", "id": "recsy6jdhmIig1YSB", "speechName": "literature", "referenceName": "literature"},
-                    {"name": "Miscellaneous", "id": "recTeqmLykygSz9gF", "speechName": "miscellaneous", "referenceName": "miscellaneous"},
-                    {"name": "Music", "id": "recdZhe6oq5wn1ksw", "speechName": "music", "referenceName": "music"},
-                    {"name": "Nature", "id": "recSKFke58Sfd9vWi", "speechName": "nature", "referenceName": "nature"},
-                    {"name": "People", "id": "recto6ILGOAYkOl8a", "speechName": "people", "referenceName": "people"},
-                    {"name": "Politics", "id": "rechKWhUV28LSeiRW", "speechName": "politics", "referenceName": "politics"},
-                    {"name": "Sports & Games", "id": "recr8BC0GYJVwjt1k", "speechName": "sports and games", "referenceName": "sports_and_games"},
-                    {"name": "Science", "id": "recKqrOdwpk5hezpL", "speechName": "science", "referenceName": "science"},
-                    {"name": "Technology", "id": "recG6oDVjpFNGrnO2", "speechName": "technology", "referenceName": "technology"},
-                    {"name": "United States", "id": "recRiC3PAYQHmMEvC", "speechName": "united states", "referenceName": "united_states"},
-                    {"name": "Tradition & Beliefs", "id": "recKh6vjk2KQLGEm7", "speechName": "tradition and beliefs", "referenceName": "tradition_and_beliefs"},
-                    {"name": "TV & Radio", "id": "recab6u8fMDf63S3k", "speechName": "tv and radio", "referenceName": "tv_and_radio"}];
+const categories = [
+    {"name": "Art & Stage",         "id": "recl0vhIOs1iSyXAu", "speechName": "art and stage",           "referenceName": "art_and_stage",           "productId": "amzn1.adg.product.4d4d4968-1e9f-40a3-a371-fcf2c92332f2"},
+    {"name": "Business World",      "id": "recAJm9yEqDRGmjRr", "speechName": "business world",          "referenceName": "business_world",          "productId": "amzn1.adg.product.45090e66-6867-4108-8984-f609e332377d"},
+    {"name": "Design",              "id": "recYsOA65RT5dQPlb", "speechName": "design",                  "referenceName": "design",                  "productId": "amzn1.adg.product.47c9a153-bb17-4534-870a-bf9b3ad76057"},
+    {"name": "Film",                "id": "recdMJliTRpWYcj41", "speechName": "film",                    "referenceName": "film",                    "productId": "amzn1.adg.product.d795f7b6-375f-4842-a70f-65df1e459b7e"},
+    {"name": "Food & Drink",        "id": "reclySZR1cRRl08jz", "speechName": "food and drink",          "referenceName": "food_and_drink",          "productId": "amzn1.adg.product.5d14e49d-8739-4d82-8a65-fa5056b6ff95"},
+    {"name": "Geography",           "id": "recXXedb9nOqy1Fkk", "speechName": "geography",               "referenceName": "geography",               "productId": "amzn1.adg.product.a24f1fea-5459-4d6c-bf9a-675e090e8707"},
+    {"name": "History",             "id": "rec6fDO7E7lXC2xe3", "speechName": "history",                 "referenceName": "history",                 "productId": "amzn1.adg.product.22492ac4-d912-465d-84ac-d9561622cc1f"},
+    {"name": "Language",            "id": "recnhcldrp4o6QwFr", "speechName": "language",                "referenceName": "language",                "productId": "amzn1.adg.product.46448efa-4023-4c35-a765-d9e3e6697a9a"},
+    {"name": "Literature",          "id": "recsy6jdhmIig1YSB", "speechName": "literature",              "referenceName": "literature",              "productId": "amzn1.adg.product.c31651fb-c855-4221-93f6-6c48798708af"},
+    {"name": "Miscellaneous",       "id": "recTeqmLykygSz9gF", "speechName": "miscellaneous",           "referenceName": "miscellaneous",           "productId": "amzn1.adg.product.a51c00b6-ddd5-4905-995c-0b55d1d10a79"},
+    {"name": "Music",               "id": "recdZhe6oq5wn1ksw", "speechName": "music",                   "referenceName": "music",                   "productId": "amzn1.adg.product.6e6a8f5d-1230-4cb5-8757-86b987f2b1a6"},
+    {"name": "Nature",              "id": "recSKFke58Sfd9vWi", "speechName": "nature",                  "referenceName": "nature",                  "productId": "amzn1.adg.product.f458868c-9fdb-428b-8820-4ce09367d375"},
+    {"name": "People",              "id": "recto6ILGOAYkOl8a", "speechName": "people",                  "referenceName": "people",                  "productId": "amzn1.adg.product.1835f3db-ddbf-445f-b713-a5a075e6afe3"},
+    {"name": "Politics",            "id": "rechKWhUV28LSeiRW", "speechName": "politics",                "referenceName": "politics",                "productId": "amzn1.adg.product.004b5d30-8b05-4bd9-b26f-bc8e782897dc"},
+    {"name": "Sports & Games",      "id": "recr8BC0GYJVwjt1k", "speechName": "sports and games",        "referenceName": "sports_and_games",        "productId": "amzn1.adg.product.d486d675-e562-494d-b6a1-e373aa6d974f"},
+    {"name": "Science",             "id": "recKqrOdwpk5hezpL", "speechName": "science",                 "referenceName": "science",                 "productId": "amzn1.adg.product.2213f263-9e0d-401c-ae40-c34924f900fd"},
+    {"name": "Technology",          "id": "recG6oDVjpFNGrnO2", "speechName": "technology",              "referenceName": "technology",              "productId": "amzn1.adg.product.d1743574-c4fe-4ec6-a968-c89ecc38a37e"},
+    {"name": "United States",       "id": "recRiC3PAYQHmMEvC", "speechName": "united states",           "referenceName": "united_states",           "productId": "amzn1.adg.product.2c40e45f-9525-45ba-bcac-9d004ee39bd2"},
+    {"name": "Tradition & Beliefs", "id": "recKh6vjk2KQLGEm7", "speechName": "tradition and beliefs",   "referenceName": "tradition_and_beliefs",   "productId": "amzn1.adg.product.c701e08e-e54e-412b-bdc9-63a38731315d"},
+    {"name": "TV & Radio",          "id": "recab6u8fMDf63S3k", "speechName": "tv and radio",            "referenceName": "tv_and_radio",            "productId": "amzn1.adg.product.76cd6f01-c886-4415-8411-7343a9ea091d"}
+];
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -172,17 +174,47 @@ const QuestionIntentHandler = {
     async handle(handlerInput) {
         console.log("HANDLED - QuestionIntentHandler");
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        const locale = handlerInput.requestEnvelope.request.locale;
         //TODO: IS THE USER IN A GAME?
         var category = getSpecificCategory(handlerInput);
-        //TODO: DOES THE USER HAVE ENTITLEMENT TO THIS CATEGORY?
-        if (category === undefined) {
-            category = getRandomCategory();
-        }
         
-        var question = await getRandomQuestion(category);
-        //var question = await getSpecificQuestion("recEBGY7blzeW0UAp");
-        sessionAttributes.currentQuestion = question.fields;
-        return await askTriviaQuestion(handlerInput, category, question);
+        if (category != undefined) {
+            const ms = handlerInput.serviceClientFactory.getMonetizationServiceClient();
+            return await ms.getInSkillProducts(locale).then(async function checkForProductAccess(result) {
+                const subscription = result.inSkillProducts.filter(record => record.referenceName === "all_categories");
+                const entitlement = result.inSkillProducts.filter(record => record.referenceName === category.referenceName);
+                //IF THE USER IS NOT ENTITLED TO THIS CATEGORY, OFFER THEM AN UPSELL.
+                if (!isEntitled(subscription, handlerInput) && !isEntitled(entitlement, handlerInput)) {    
+                    var upsellMessage = await getRandomResponse("Upsell", locale);
+                    upsellMessage = upsellMessage.fields.VoiceResponse.replace("XXXXXXXXXX", category.speechName);
+
+                    return handlerInput.responseBuilder
+                        .addDirective({
+                            type: 'Connections.SendRequest',
+                            name: 'Upsell',
+                            payload: {
+                            InSkillProduct: {
+                                productId: entitlement[0].productId,
+                            },
+                            upsellMessage,
+                            },
+                            token: 'correlationToken',
+                        })
+                        .getResponse();
+                }
+                else {
+                    var question = await getRandomQuestion(category);
+                    sessionAttributes.currentQuestion = question.fields;
+                    return await askTriviaQuestion(handlerInput, category, question);
+                }
+            });
+        }
+        else {
+            category = getRandomCategory();
+            var question = await getRandomQuestion(category);
+            sessionAttributes.currentQuestion = question.fields;
+            return await askTriviaQuestion(handlerInput, category, question);
+        }
     }
 };
 
@@ -311,6 +343,78 @@ const ErrorHandler = {
     }
 };
 
+//THIS HANDLES THE CONNECTIONS.RESPONSE EVENT AFTER AN UPSELL OCCURS.
+const UpsellResponseHandler = {
+    canHandle(handlerInput) {
+        console.log("CAN HANDLE - UPSELL RESPONSE HANDLER");
+        return handlerInput.requestEnvelope.request.type === "Connections.Response" &&
+               handlerInput.requestEnvelope.request.name === "Upsell";
+    },
+    async handle(handlerInput) {
+        console.log("HANDLE - UPSELL RESPONSE HANDLER");
+        const locale = handlerInput.requestEnvelope.request.locale;
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        const ms = handlerInput.serviceClientFactory.getMonetizationServiceClient();
+        const productId = handlerInput.requestEnvelope.request.payload.productId;
+        var actionQuery = await getRandomResponse("ActionQuery", locale);
+        actionQuery = actionQuery.fields.VoiceResponse;
+
+        return ms.getInSkillProducts(locale).then(async function(res) {
+            let product = res.inSkillProducts.find(record => record.productId == productId);
+            var category = categories.find(o => o.productId === productId)
+
+            if (handlerInput.requestEnvelope.request.status.code == 200) {
+                if (handlerInput.requestEnvelope.request.payload.purchaseResult == 'DECLINED') {
+                    var speakText = "";
+                    //IF SUBSCRIPTION
+                    if (productId === "amzn1.adg.product.f6a25e4f-de02-403e-b848-642ab5ce66a4") {
+                        speakText = "If you change your mind, just ask me about the subscription! " + actionQuery;
+                    }
+                    //ELSE CATEGORY
+                    else {
+                        speakText = "You can always ask for a random question, but to get questions from a specific category, you need to purchase that category or the TKO subscription. " + actionQuery;
+                    }
+
+                    const repromptText = actionQuery;
+                    return handlerInput.responseBuilder
+                        .speak(speakText)
+                        .reprompt(repromptText)
+                        .getResponse();
+                }
+                else if (handlerInput.requestEnvelope.request.payload.purchaseResult == 'ACCEPTED') {
+                    var speakText = "";
+                    //IF SUBSCRIPTION
+                    if (productId === "amzn1.adg.product.f6a25e4f-de02-403e-b848-642ab5ce66a4") {
+                        speakText = "You have successfully unlocked the TKO Trivia subscription!  You can now ask for questions from any of the categories, and the correct answers will be shown after every question, even if you get it wrong!";
+                    }
+                    //ELSE CATEGORY
+                    else {
+                        speakText = "You can now say things like, ask me a " + category.speechName + " question, any time!";
+                    }
+
+                    var question = await getRandomQuestion(category);
+                    sessionAttributes.currentQuestion = question.fields;
+                    return await askTriviaQuestion(handlerInput, category, question, 0, speakText);
+                }
+                else if (handlerInput.requestEnvelope.request.payload.purchaseResult == 'ERROR') {
+                    const speakText = actionQuery;
+                    const repromptText = actionQuery;
+                    return handlerInput.responseBuilder
+                        .speak(speakText)
+                        .reprompt(repromptText)
+                        .getResponse();
+                }
+            }
+            else    {
+                // Something failed.
+                console.log('Connections.Response indicated failure. error:' + handlerInput.requestEnvelope.request.status.message);
+                return handlerInput.responseBuilder
+                    .speak("There was an error handling your purchase request. Please try again or contact us for help.")
+                    .getResponse();
+            }
+        });
+   }
+};
 
 async function askTriviaQuestion(handlerInput, category, question, ordinal = 0, speech = "") {
     console.log("ASKING TRIVIA QUESTION.")
@@ -415,6 +519,14 @@ function getRandomItem(items) {
 
 function getRandom(min, max){
     return Math.floor(Math.random() * (max-min+1)+min);
+}
+
+function isProduct(product) {
+    return product && product.length > 0;
+}
+
+function isEntitled(product) {
+    return isProduct(product) && product[0].entitled === 'ENTITLED';
 }
 
 function isAnswerCorrect(handlerInput){
@@ -526,8 +638,9 @@ exports.handler = dashbot.handler(skillBuilder
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         RepeatIntentHandler,
+        UpsellResponseHandler,
         SessionEndedRequestHandler,
-        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+        IntentReflectorHandler // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
         ) 
     .addErrorHandlers(ErrorHandler)
     .addRequestInterceptors(RequestLog)
